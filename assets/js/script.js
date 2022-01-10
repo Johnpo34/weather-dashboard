6e4206331c14dbc95cab11f50b28beef9c49e772
John Pohl <johnpohl34@gmail.com>
2022-01-10 10:06:26 -0500

    added codes

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+// current - https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=0a140ffac63fb307b321a0a3034253b3&units=imperial
+
+// 5day - https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=0a140ffac63fb307b321a0a3034253b3&units=imperial
+
+var searchBtn = document.querySelector('.search-btn');
+var fiveDayContainer = document.querySelector('.five-day-container')
+
+searchBtn.addEventListener('click', function() {
+    var city = document.querySelector('.input-container').value
+    getCurrentWeather(city)
+})
+
+function getCurrentWeather(value) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ value + '&appid=0a140ffac63fb307b321a0a3034253b3&units=imperial')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+
+        var lat = data.coord.lat
+        var lon = data.coord.lon
+        getFiveDay(lat, lon)
+
+        document.querySelector('.city-name').textContent = data.name
+
+    })
+}
+
+function getFiveDay(lat, lon) {
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+ lat + '&lon='+ lon +'&appid=0a140ffac63fb307b321a0a3034253b3&units=imperial')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+
+        for(var i = 0; i < 5; i++) {
+            var card = document.createElement('div')
+            fiveDayContainer.append(card)
+
+            var day = document.createElement('h2')
+            day.setAttribute('class', 'day')
+            day.textContent = moment().add(i + 1, 'days').format('dddd')
+            card.append(day)
+
+            var fiveDayTemp = document.createElement('p')
+            fiveDayTemp.textContent = 'Temp: ' + data.daily[i].temp.day + ' F'
+            card.append(fiveDayTemp)
+        }
+    })
+}
